c048d8b6708e0d78f03d89d188d46396d2654980
sm-stack <jeon0015@naver.com>
2023-07-27 13:03:09 +0900

    Modify function visibility to external

--- contracts/Week1/ArrayDelete.sol
+++ contracts/Week1/ArrayDelete.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.18;
 
 contract ArrayDelete {
 
-    function deleteIndexMemory(uint256[] calldata arr, uint256 index) public pure returns (uint256[] memory) {
+    function deleteIndexMemory(uint256[] calldata arr, uint256 index) external pure returns (uint256[] memory) {
         require(index < arr.length, "Index Out of Range");
         uint256[] memory new_arr = new uint256[](arr.length - 1);
     
